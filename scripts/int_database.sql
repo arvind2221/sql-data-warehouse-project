7423d482916057b6f55c3e6fbe80fa4c1514c739
arvind2221 <arvindsingh2221@gmail.com>
2025-04-20 00:44:07 +0530

    Create int_database.sql

---
+++ scripts/int_database.sql
@@ -0,0 +1,14 @@
+use master;
+GO
+--create datawarehouse database
+CREATE DATABASE Datawarehouse;
+
+USE Datawarehouse;
+GO
+--creating the schemas
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
